25153bbc69d2241ee7c8e230b8bee1f8d979a77e
Iskren Vanev <iskren.vanev@inensia.com>
2024-06-08 21:07:22 +0300

    Added dropdown menu for the Suppliers. Changed the OrderVM, added new migrations and some other updates!

--- StohlDivan.Models/Supplier.cs
+++ StohlDivan.Models/Supplier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,6 @@ namespace StohlDivan.Models
 
         public string SupplierName { get; set; }
 
-        public OrderHeader OrderHeader { get; set; }
+       
     }
 }
