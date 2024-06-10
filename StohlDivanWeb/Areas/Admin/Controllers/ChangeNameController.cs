8b4ebc61610b4285eb63e53b4bd026519bccf3fd
Iskren Vanev <iskren.vanev@inensia.com>
2024-06-10 22:25:39 +0300

    Now you can change the name of the website!

--- StohlDivanWeb/Areas/Admin/Controllers/ChangeNameController.cs
+++ StohlDivanWeb/Areas/Admin/Controllers/ChangeNameController.cs
@@ -11,12 +11,34 @@ namespace StohlDivanWeb.Areas.Admin.Controllers
     {
 
         private readonly IUnitOfWork _unitOfWork;
-        public ChangeNameController(IUnitOfWork unitOfWork)
+        private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _env;
+        public ChangeNameController(IUnitOfWork unitOfWork, IConfiguration configuration, IHostEnvironment env)
         {
             _unitOfWork = unitOfWork;
+            _configuration = configuration;
+            _env = env;
         }
         public IActionResult Index()
         {
+            var siteName = _configuration["WebsiteSettings:SiteName"];
+            ViewBag.SiteName = siteName;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string siteName)
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, "appsettings.json");
+
+            var json = await System.IO.File.ReadAllTextAsync(filePath);
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            jsonObj["WebsiteSettings"]["SiteName"] = siteName;
+
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            await System.IO.File.WriteAllTextAsync(filePath, output);
+
+            ViewBag.SiteName = siteName;
+            TempData["success"] = "Website name changed successfully";
             return View();
         }
     }
